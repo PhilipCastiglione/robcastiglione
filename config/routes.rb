68bb3acd47c24486e8b947c9fb210658d0748269
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2015-12-24 08:26:41 +0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,9 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  devise_for :users
+  resources :films, :filmcredits, :sounds, :stills 
   get '/' => 'films#index', :as => 'root'
-  post '/films' => 'films#create'
-  put '/films' => 'films#update'
-  delete '/films' => 'films#destroy'
-
-  post '/filmcredits' => 'film_credits#create'
-  put '/filmcredits' => 'film_credits#update'
-  delete '/filmcredits' => 'film_credits#destroy'
-
-  get '/sounds' => 'sounds#index'
-  post '/sounds' => 'sounds#create'
-  put '/sounds' => 'sounds#update'
-  delete '/sounds' => 'sounds#destroy'
-
-  get '/stills' => 'stills#index'
-  post '/stills' => 'stills#create'
-  put '/stills' => 'stills#update'
-  delete '/stills' => 'stills#destroy'
 
   get '/about' => 'static#about'
   get '/contact' => 'static#contact'
-
 end
