d0f1fc1e91e6ae9410350ecd15849b0b4a16e459
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2015-12-24 08:31:57 +0800

    added rails admin configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
+  devise_for :users
+
   resources :films, :filmcredits, :sounds, :stills 
   get '/' => 'films#index', :as => 'root'
 
